1124b9d5980b9acc2e69be971762ced9e1062d26
hexonZhang <308826842@qq.com>
2017-07-31 16:50:31 +0800

    将代码移到 componentDidUpdate

--- react-todolist/todolist-storage/src/App.js
+++ react-todolist/todolist-storage/src/App.js
@@ -46,11 +46,14 @@ class App extends Component {
     );
   }
 
+  componentDidUpdate() {
+    localStore.save('todoList', this.state.todoList);
+  }
+
   toggle(e, todo) {
     todo.status = todo.status === 'completed' ? '' : 'completed';
     this.setState(this.state);
-    localStore.save('todoList', this.state.todoList);
-  }
+   }
 
   changeTitle(event) {
     console.log('hhh');
@@ -73,14 +76,13 @@ class App extends Component {
       newTodo: '',
       todoList: this.state.todoList
     });
-    localStore.save('todoList', this.state.todoList);
-  }
+   }
 
   delete(event, todo) {
     todo.deleted = true;
     this.setState(this.state);
-    localStore.save('todoList', this.state.todoList);
-  }
+   }
+
 }
 
 export default App;
