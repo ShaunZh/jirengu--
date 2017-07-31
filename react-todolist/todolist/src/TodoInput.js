6149a964b72cb83def699f2f9a5e1914d192bc36
hexonZhang <308826842@qq.com>
2017-07-31 11:43:06 +0800

    监听回车事件

--- react-todolist/todolist/src/TodoInput.js
+++ react-todolist/todolist/src/TodoInput.js
@@ -2,13 +2,18 @@
 * @Author: Marte
 * @Date:   2017-07-31 10:09:16
 * @Last Modified by:   Marte
-* @Last Modified time: 2017-07-31 11:35:53
+* @Last Modified time: 2017-07-31 11:41:36
 */
 
 import React, {Component} from 'react';
 
 export default class TodoInput extends Component {
   render() {
-    return <input type="text" defaultValue = {this.props.content} />
+    return <input type="text" defaultValue = {this.props.content} onKeyPress={this.submit}/>
+  }
+  submit(e) {
+    if (e.key === 'Enter') {
+      console.log('用户按回车了');
+    }
   }
 }
