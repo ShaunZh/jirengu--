738520dc17a37c24beb64583260a56c31c8b2e39
hexonZhang <308826842@qq.com>
2017-07-31 13:17:42 +0800

    this.submit.bind(this)

--- react-todolist/todolist/src/TodoInput.js
+++ react-todolist/todolist/src/TodoInput.js
@@ -2,18 +2,18 @@
 * @Author: Marte
 * @Date:   2017-07-31 10:09:16
 * @Last Modified by:   Marte
-* @Last Modified time: 2017-07-31 11:41:36
+* @Last Modified time: 2017-07-31 13:15:49
 */
 
 import React, {Component} from 'react';
 
 export default class TodoInput extends Component {
   render() {
-    return <input type="text" defaultValue = {this.props.content} onKeyPress={this.submit}/>
+    return <input type="text" defaultValue = {this.props.content} onKeyPress={this.submit.bind(this)}/>
   }
   submit(e) {
     if (e.key === 'Enter') {
-      console.log('用户按回车了');
+      this.props.onSubmit.call();
     }
   }
 }
