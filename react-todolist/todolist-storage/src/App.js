963265050ef98c77170a3952f14b7c27caf4103f
hexonZhang <308826842@qq.com>
2017-08-01 14:47:07 +0800

    注册成功就关闭 UserDialog

--- react-todolist/todolist-storage/src/App.js
+++ react-todolist/todolist-storage/src/App.js
@@ -54,14 +54,12 @@ class App extends Component {
         <ol className="todoList">
           {todos}
         </ol>
-        <UserDialog onSignUp={this.onSignUp.bind(this)}/>
+        {this.state.user.id ? null : <UserDialog onSignUp={this.onSignUp.bind(this)}/>}
       </div>
     );
   }
 
   onSignUp(user) {
-    this.state.user = user;
-    this.setState(this.state);
     let stateCopy = JSON.parse(JSON.stringify(this.state));
     stateCopy.user = user;
     this.setState(stateCopy);
